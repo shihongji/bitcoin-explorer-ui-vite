09d0c6d646d67de8e83dc110aa48065603341c47
hongji <shihongji21@gmail.com>
2024-07-03 16:19:23 +0800

    ip

--- src/components/LatestBlocks.tsx
+++ src/components/LatestBlocks.tsx
@@ -16,7 +16,7 @@ const LatestBlocks: React.FC = () => {
 
   const fetchBlocks = async () => {
     try {
-      const response = await fetch("http://localhost:3001/blockchain_metrics");
+      const response = await fetch("http://34.31.232.228:3001/blockchain_metrics");
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
